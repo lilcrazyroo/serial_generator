e964e0eaf968f03f49a73e04a11df1eaa2e5bf37
Илья Киселёв <kis.ilya92@gmail.com>
2022-05-13 01:36:36 +0300

    added new examples

---
+++ examples/example.cpp
@@ -0,0 +1,21 @@
+#include "serialization.h"
+#include "serial_endian.h"
+#include "serial_trace.h"
+#include "serial_exception.h"
+#include "serial_precompile.h"
+
+int main() {
+    Article article { "Article1", {
+        Article{ "Article2" },
+        Article{ "Article3" }
+    } };
+
+    try {
+        using StringWrapper = memserial::StringWrapper< memserial::BigEndian >;
+        auto bytes = memserial::serialize< StringWrapper >( article );
+        memserial::print( memserial::parse< Article >( bytes ), std::cout );
+    }
+    catch ( memserial::SerialException& ex ) {
+        std::cerr << ex.what();
+    }
+}
--- examples/example3.cpp
+++ examples/example3.cpp
@@ -1,21 +1,99 @@
-#include "serialization.h"
-#include "serial_endian.h"
-#include "serial_trace.h"
-#include "serial_exception.h"
-#include "serial_precompile.h"
+#include "serial_forward.h"
+
+#include <ctime>
+
+struct Head {
+    uint64_t size;
+    uint64_t type;
+};
+
+struct [[gnu::packed]] Meta {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    int32_t v0 : 7;
+    int32_t v1 : 28;
+    int32_t v2 : 29;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+    int32_t v2 : 29;
+    int32_t v1 : 28;
+    int32_t v0 : 7;
+#endif
+};
+
+std::ostream& operator<<( std::ostream& out, const Meta& meta ) {
+    return out << meta.v0 << " " << meta.v1 << " " << meta.v2;
+}
+
+struct Payload {
+    std::chrono::system_clock::time_point time;
+    std::vector< std::complex< double > > quads;
+
+    char* ctime() const {
+        std::time_t tp = std::chrono::system_clock::to_time_t( time );
+        return std::ctime( &tp );
+    }
+};
+
+std::ostream& operator<<( std::ostream& out, const Payload& payload ) {
+    out << payload.ctime();
+    memserial::print( payload, out );
+    return out;
+}
+
+struct Package {
+    Head m_head;
+    std::array< char, sizeof( Meta ) > m_meta;
+    std::string m_payload;
+
+    Head& head() {
+        return m_head;
+    }
+
+    Meta& meta() {
+        return reinterpret_cast< Meta& >( *m_meta.data() );
+    }
+
+    Payload payload() {
+        using Endian = memserial::StringWrapper< memserial::BigEndian >;
+        return memserial::parse< Payload >( static_cast< Endian& >( m_payload ) );
+    }
+};
+
+SERIAL_TYPE_INFO( Package )
+SERIAL_TYPE_INFO( Payload )
 
 int main() {
-    Article article { "Article1", {
-        Article{ "Article2" },
-        Article{ "Article3" }
-    } };
+    char bytes[ 1024 ];
+
+    {
+        Payload payload;
+        payload.time = std::chrono::system_clock::now();
+        payload.quads = { { 0, 1 }, { 1, 0 }, { 1, 1 } };
+
+        Package package;
+        package.meta() = { 42, 69, 777 };
+        package.m_payload = memserial::serialize< memserial::StringWrapper< memserial::BigEndian > >( payload );
+        package.m_head.type = memserial::ident< Payload >();
+        package.m_head.size = memserial::size( package );
+
+        memserial::SerialView view( bytes, package.m_head.size );
+        memserial::serialize( view, package );
+    }
 
     try {
-        using StringWrapper = memserial::StringWrapper< memserial::BigEndian >;
-        auto bytes = memserial::serialize< StringWrapper >( article );
-        memserial::print( memserial::parse< Article >( bytes ), std::cout );
+        Head head;
+        memserial::parse( head, memserial::SerialView( bytes, memserial::size( head ) ) );
+        std::cout << "Type: " << head.type << "\n";
+
+        Package package = memserial::parse< Package >( memserial::SerialView( bytes, head.size ) );
+        std::cout << "Meta: " << package.meta() << "\n";
+
+        if ( head.type == memserial::ident< Payload >() )
+            std::cout << package.payload() << "\n";
     }
+
     catch ( memserial::SerialException& ex ) {
-        std::cout << ex.what();
+        std::cerr << ex.what();
     }
+
+
 }
